2e357fc4669069900749f801c4c989cfffdb0271
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2024-05-07 17:54:27 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,43 +1,50 @@
-import java.util.*;
 import java.io.*;
-import java.math.*;
+import java.util.*;
 
-public class Main {
-    static int n;
-    static int m;
-    static int[][] arr;
-    static int count = 0;
+public class Main{
+
+    static int[][] graph;
+    static int result = 0;
     static boolean[] visited;
+    static int n;
+    static int p;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        arr = new int[n + 1][n + 1];
-        visited = new boolean[n + 1];
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n1 = Integer.parseInt(st.nextToken());
-            int n2 = Integer.parseInt(st.nextToken());
-            arr[n1][n2] = arr[n2][n1] = 1;
-        }
-        System.out.println(bfs(1));
-    }
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static int bfs(int k) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(k);
-        visited[k] = true;
-        while (!q.isEmpty()) {
-            int temp = q.poll();
-            for (int i = 1; i <= n; i++) {
-                if (arr[temp][i] == 1 && !visited[i]) {
-                    q.offer(i);
-                    visited[i] = true;
-                    count++;
-                }
+        n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        p = Integer.parseInt(st.nextToken());
+
+        graph = new int[n+1][n+1];
+        visited = new boolean[n + 1];
+
+        for(int i = 0; i <= n; i++){
+            visited[i] = false;
+            for(int j = 0; j <= n; j++){
+                graph[i][j] = 0;
+
             }
         }
-        return count;
+        for(int i = 0; i < p; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = graph[b][a] = 1;
+        }
+
+        dfs(1);
+        System.out.println(result - 1);
+    }
+
+    public static void dfs(int start){
+        visited[start] = true;
+        result++;
+        for(int i = 0; i <= n; i++){
+            if(graph[start][i] == 1 && !visited[i]){
+                dfs(i);
+            }
+        }
     }
 }
