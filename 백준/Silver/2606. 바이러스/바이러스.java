144029604c7f9935f7d549a3e944da0349da2abd
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2024-05-11 13:41:58 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main{
 
     static int[][] graph;
+    static LinkedList<Integer>[] adjList;
     static int result = 0;
     static boolean[] visited;
     static int n;
@@ -17,11 +18,15 @@ public class Main{
         st = new StringTokenizer(br.readLine());
         p = Integer.parseInt(st.nextToken());
 
+        //인접행렬
         graph = new int[n+1][n+1];
+        //인접리스트
+        adjList = new LinkedList[n+1];
         visited = new boolean[n + 1];
 
         for(int i = 0; i <= n; i++){
             visited[i] = false;
+            adjList[i] = new LinkedList<>();
             for(int j = 0; j <= n; j++){
                 graph[i][j] = 0;
 
@@ -31,11 +36,14 @@ public class Main{
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+            //인접리스트 초기화
             graph[a][b] = graph[b][a] = 1;
+            //인접행렬 초기화
+            adjList[a].add(b);
+            adjList[b].add(a);
         }
-
-        dfs(1);
-        System.out.println(result - 1);
+        dfs_2(1);
+        System.out.println(result);
     }
 
     public static void dfs(int start){
@@ -44,6 +52,16 @@ public class Main{
         for(int i = 0; i <= n; i++){
             if(graph[start][i] == 1 && !visited[i]){
                 dfs(i);
+            }
+        }
+    }
+
+    public static void dfs_2(int start){
+        visited[start] = true;
+        for(int i : adjList[start]){
+            if(!visited[i]){
+                result++;
+                dfs_2(i);
             }
         }
     }
