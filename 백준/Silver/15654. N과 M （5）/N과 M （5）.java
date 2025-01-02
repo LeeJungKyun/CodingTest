b7fd172b1c8565866e844209223331cd68f5ced3
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-01-02 11:32:34 +0900

    [Silver III] Title: N과 M (5), Time: 2044 ms, Memory: 84476 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static int[] result;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        result = new int[m];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        backTrack(0);
+    }
+
+    public static void backTrack(int depth) {
+        if (depth == m) {
+            for (int val : result) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                backTrack(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
