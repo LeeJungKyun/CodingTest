cf4c3347df3d10612b23d177d881b0d6d1f17f06
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-01-13 14:01:45 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Node {
+        char value;
+        Node left;
+        Node right;
+
+        public Node(char value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static int n;
+    static Node[] tree;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        tree = new Node[n + 1];
+
+        //트리 만들기
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char parent = st.nextToken().charAt(0);
+            char leftNode = st.nextToken().charAt(0);
+            char rightNode = st.nextToken().charAt(0);
+
+            if (tree[parent - 'A'] == null) {
+                tree[parent - 'A'] = new Node(parent);
+            }
+
+            if (leftNode != '.') {
+                tree[leftNode - 'A'] = new Node(leftNode);
+                tree[parent - 'A'].left = tree[leftNode - 'A'];
+            }
+
+            if (rightNode != '.') {
+                tree[rightNode - 'A'] = new Node(rightNode);
+                tree[parent - 'A'].right = tree[rightNode - 'A'];
+            }
+        }
+
+        preorder(tree[0]);
+        System.out.println();
+
+        inorder(tree[0]);
+        System.out.println();
+
+        postorder(tree[0]);
+        System.out.println();
+    }
+
+    public static void preorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.value);
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    public static void inorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left);
+        System.out.print(node.value);
+        inorder(node.right);
+    }
+
+    public static void postorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.value);
+    }
+}
