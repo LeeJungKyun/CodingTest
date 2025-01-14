23aee9ac347ce56d4340d2a73d681d172b13bca9
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-01-14 13:55:10 +0900

    [Silver I] Title: 스티커, Time: 632 ms, Memory: 110480 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int T;
+    static int n;
+    static int[][] arr;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        for (int testCase = 0; testCase < T; testCase++) {
+
+            n = Integer.parseInt(br.readLine());
+            arr = new int[2][n + 1];
+            dp = new int[2][n + 1];
+
+            for (int i = 0; i < 2; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= n; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
+
+            for (int j = 2; j <= n; j++) {
+                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + arr[0][j];
+                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + arr[1][j];
+            }
+
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
+        }
+    }
+}
