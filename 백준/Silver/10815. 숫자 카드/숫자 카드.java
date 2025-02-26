988f14914eac1ce7635c5a55ba1f715ae5d54c2a
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-02-26 10:04:14 +0900

    [Silver V] Title: 숫자 카드, Time: 892 ms, Memory: 135708 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,49 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
-    static int[] arr;
-    static int[] compare;
-
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
         n = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
 
-        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
 
         m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-
-            System.out.print(binarySearch(num) + " ");
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                sb.append(1).append(' ');
+            }
+            else sb.append(0).append(' ');
         }
 
-    }
+        System.out.println(sb);
 
-    public static int binarySearch(int num) {
-        int left = 0;
-        int right = n - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int midValue = arr[mid];
-
-            if (num > midValue) {
-                left = mid + 1;
-            } else if (num < midValue) {
-                right = mid - 1;
-            } else return 1;
-        }
-        return 0;
     }
 }
