74331c36a6978891bc9b62f18203a2380e1dbbf1
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-06-16 09:32:16 +0900

    [Silver II] Title: 키로거, Time: 728 ms, Memory: 135360 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            LinkedList<Character> list = new LinkedList<>();
+            ListIterator<Character> iter = list.listIterator();
+
+            for (char c : input.toCharArray()) {
+                switch (c) {
+                    case '<':
+                        if (iter.hasPrevious()) {
+                            iter.previous();
+                        }
+                        break;
+                    case '>':
+                        if (iter.hasNext()) {
+                            iter.next();
+                        }
+                        break;
+                    case '-':
+                        if (iter.hasPrevious()) {
+                            iter.previous();
+                            iter.remove();
+                        }
+                        break;
+                    default:
+                        iter.add(c);
+                        break;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (char c : list) {
+                sb.append(c);
+            }
+            System.out.println(sb.toString());
+        }
+
+        br.close();
+    }
+}
