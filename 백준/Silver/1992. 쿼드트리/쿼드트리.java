aa9c54f59182a5a18bf37cd95aa243819a0b3cd5
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-08-14 13:00:30 +0900

    [Silver I] Title: 쿼드트리, Time: 72 ms, Memory: 12044 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,18 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
+/**
+ * @author 이정균
+ * 
+ * 1. n입력
+ * 2. 배열 입력
+ * 3. divide재귀함수 @param x, y, size
+ * 	3-1. 색깔 확인해서 x ~ x + size, y ~ y + size가 다 같은 색이면 해당 색깔 출력
+ * 	3-2. 다르면 ( 출력
+ * 	3-3. size /= 2해서 재귀
+ *  3-4. 다 끝나면 ) 출력
+ */
 public class Main {
     static int n;
     static int[][] arr;
-    static StringBuilder sb;
+    static BufferedReader br;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	br = new BufferedReader(new InputStreamReader(System.in));
+    	//1. n입력
         n = Integer.parseInt(br.readLine());
-
+        //2. 배열 입력
         arr = new int[n][n];
-        sb = new StringBuilder();
-
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < n; j++) {
@@ -20,6 +29,7 @@ public class Main {
             }
         }
 
+        //3. divide재귀함수 @param x, y, size
         divide(0, 0, n);
 
         System.out.println(sb);
@@ -27,6 +37,7 @@ public class Main {
     }
 
     public static void divide(int x, int y, int size) {
+    	//3-1. 색깔 확인해서 x ~ x + size, y ~ y + size가 다 같은 색이면 해당 색깔 출력
         if (checkColor(x, y, size)) {
             if (arr[x][y] == 1) {
                 sb.append(1);
@@ -34,18 +45,22 @@ public class Main {
                 sb.append(0);
             }
         } else {
+        	//3-2. 다르면 ( 출력
             sb.append('(');
+            //3-3. size /= 2해서 재귀
             int newSize = size / 2;
 
             divide(x, y, newSize); //좌상
             divide(x, y + newSize, newSize); //우상
             divide(x + newSize, y, newSize); //좌하
             divide(x + newSize, y + newSize, newSize); //우하
+            //3-4. 다 끝나면 ) 출력
             sb.append(')');
 
         }
     }
 
+    //x ~ x + size, y ~ y + size가 다 같은 색인지 확인
     public static boolean checkColor(int x, int y, int size) {
         int color = arr[x][y];
         for (int i = x; i < x + size; i++) {
