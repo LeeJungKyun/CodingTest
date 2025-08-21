4ca16e59e0326d45eefc017cf47784ad02c6e263
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-08-21 12:14:26 +0900

    [Gold V] Title: 암호 만들기, Time: 64 ms, Memory: 12784 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -3,38 +3,38 @@ import java.util.*;
 
 public class Main {
     static char[] alphabet, result;
-    static int l, c;
+    static int length, characterCount;
     static final String vowels = "aeiou";
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        l = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        length = Integer.parseInt(st.nextToken());
+        characterCount = Integer.parseInt(st.nextToken());
 
-        alphabet = new char[c];
-        result = new char[l];
+        alphabet = new char[characterCount];
+        result = new char[length];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < c; i++) {
+        for (int i = 0; i < characterCount; i++) {
             alphabet[i] = st.nextToken().charAt(0);
         }
 
         Arrays.sort(alphabet);
-        dfs(0, 0);
+        makePassword(0, 0);
         System.out.println(sb);
 
     }
 
-    public static void dfs(int depth, int idx) {
-        if (depth == l) {
+    public static void makePassword(int depth, int idx) {
+        if (depth == length) {
             if (isValid(result)) {
                 sb.append(new String(result)).append('\n');
             }
             return;
         }
-        for (int i = idx; i < c; i++) {
+        for (int i = idx; i < characterCount; i++) {
             result[depth] = alphabet[i];
-            dfs(depth + 1, i + 1);
+            makePassword(depth + 1, i + 1);
         }
     }
 
