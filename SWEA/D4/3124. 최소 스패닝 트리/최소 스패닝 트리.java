083a9e493e2ae76b7e38e2b2a61632f711c99085
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-08-27 09:34:13 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,040 ms, Memory: 188,104 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,23 +3,24 @@ import java.util.*;
 
 /**
  * @author 이정균
- * 
+ *
+ * 이 코드는 프림(Prim) 알고리즘을 사용하여 최소 스패닝 트리(MST)의 총 비용을 계산합니다.
+ * 그래프는 인접 리스트(Adjacency List) 형태로 표현됩니다.
+ *
  * 1. 테스트케이스 입력
  * 2. 정점의 개수 v, 간선의 개수 e 입력
- * 3. e 개의 줄에 a b c 입력 a 와 b를 연결하는게 c비용
- * 4. 간선 정렬
- * 5. 필요 변수 초기화
- * 6. 간선 순회하면서 안합쳐져 있으면 합치기
- * 6. 
+ * 3. e개의 줄에 a, b, c(시작, 끝, 가중치)를 입력받아 인접 리스트 초기화
+ * 4. 필요한 변수(visited 배열, PriorityQueue) 초기화
+ * 5. 프림 알고리즘 실행
+ * 6. 결과 출력
  *
  */
 public class Solution {
 	static class Edge implements Comparable<Edge> {
-		int start, dest, weight;
+		int dest, weight;
 
-		public Edge(int start, int dest, int weight) {
+		public Edge(int dest, int weight) {
 			super();
-			this.start = start;
 			this.dest = dest;
 			this.weight = weight;
 		}
@@ -32,8 +33,8 @@ public class Solution {
 	static int testCase;
 	static int v, e;
 	static long result;
-	static int[] parent;
-	static ArrayList<Edge> graph;
+	static ArrayList<Edge>[] adjList; // 인접 리스트 선언
+	static boolean[] visited;
 	static BufferedReader br;
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
@@ -50,54 +51,71 @@ public class Solution {
 			v = Integer.parseInt(st.nextToken());
 			e = Integer.parseInt(st.nextToken());
 			
-			//3. e개의 줄에 a b c 입력
-			graph = new ArrayList<>();
+			// 인접 리스트 초기화
+			adjList = new ArrayList[v + 1];
+			for (int i = 1; i <= v; i++) {
+				adjList[i] = new ArrayList<>();
+			}
+			
+			//3. e개의 줄에 a b c 입력하면서 인접 리스트 초기화
 			for(int i = 0; i < e; i++) {
 				st = new StringTokenizer(br.readLine());
 				int start = Integer.parseInt(st.nextToken());
 				int end = Integer.parseInt(st.nextToken());
 				int weight = Integer.parseInt(st.nextToken());
 				
-				graph.add(new Edge(start, end, weight));
+				// 양방향 그래프이므로 양쪽에 모두 추가
+				adjList[start].add(new Edge(end, weight));
+				adjList[end].add(new Edge(start, weight));
 			}
 			
-			//4. 간선 정렬
-			Collections.sort(graph);
 			
-			//5. 필요 변수 초기화
-			parent = new int[v + 1];
-			for(int i = 1; i <= v; i++)
-				parent[i] = i;
-			result = 0;
+			//4. 필요 변수 초기화
+			visited = new boolean[v + 1];
 			
-			//6. 간선 순회하면서 안합쳐져 있으면 합치기
-			int edgeCount = 0;
-			for(Edge edge : graph) {
-				if(find(edge.start) != find(edge.dest)) {
-					result += edge.weight;
-					edgeCount++;
-					union(edge.start, edge.dest);
-					if(edgeCount == v - 1)
-						break;
-				}
-			}
+			//5. 프림 알고리즘
+			result = prim();
 			
 			sb.append('#').append(tc).append(' ').append(result).append('\n');
 		}
 		System.out.println(sb);
 	}
 	
-	public static int find(int x) {
-		if(x == parent[x])
-			return x;
-		else return parent[x] = find(parent[x]);
-	}
-	
-	public static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
+	public static long prim() {
+		long minCost = 0;
 		
-		if(x != y)
-			parent[y] = x;
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		
+		int count = 0;
+		
+		//1번 노드부터 시작, 가중치는 0으로 시작
+		pq.offer(new Edge(1, 0));
+		
+		while(!pq.isEmpty()) {
+			Edge curEdge = pq.poll();
+			int curDest = curEdge.dest;
+			int curWeight = curEdge.weight;
+			
+			// 이미 방문한 노드면 건너뛰기
+			if(visited[curDest])
+				continue;
+			
+			// 현재 노드를 MST에 포함시키고 비용 추가
+			visited[curDest] = true;
+			minCost += curWeight;
+			count++;
+			
+			// 모든 정점(V)이 연결되면 종료
+			if(count == v)
+				break;
+				
+			// 현재 정점에 연결된 모든 간선을 우선순위 큐에 추가
+			for(Edge nextEdge : adjList[curDest]) {
+				if(!visited[nextEdge.dest]) {
+					pq.offer(nextEdge);
+				}
+			}
+		}
+		return minCost;
 	}
 }
