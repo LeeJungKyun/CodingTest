0dbca9a486527c1e9d0cc0528a8ccb4a47794e3e
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-01 13:20:46 +0900

    [Gold IV] Title: 연구소, Time: 888 ms, Memory: 296480 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,101 +1,126 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
- * 백준 14502 연구소
- * 사용 로직
- * 1. DFS
- * 2. 백트래킹
+ * @author 이정균
+ * 
+ * 1. N, M입력
+ * 2. 배열 입력
+ * 3. 백트래킹으로 벽 세울 곳 정하고, 3개 다 세웠다면 바이러스 퍼뜨리고 안전구역 개수 구하기
+ *
  */
 public class Main {
-    static class Pair {
-        private int x;
-        private int y;
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	static class Point{
+		int x, y;
 
-    static int[][] board;
-    static int n,m;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int max_safe = Integer.MIN_VALUE;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	static final int WALL_SIZE = 3;
+	static int n, m;
+	static int maxSafetySize = Integer.MIN_VALUE;
+	static int[][] arr;
+	static ArrayList<Point> virusList;
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, -1, 0, 1};
+	static BufferedReader br;
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        board = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        dfs(0);
-
-        System.out.println(max_safe);
-    }
-
-    public static void dfs(int wall) {
-        if (wall == 3) {
-            bfs();
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 0) {
-                    board[i][j] = 1;
-                    dfs(wall + 1);
-                    board[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    public static void bfs() {
-        Queue<Pair> virus = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 2)
-                    virus.add(new Pair(i, j));
-            }
-        }
-        int[][] new_board = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            new_board[i] = board[i].clone();
-        }
-        while (!virus.isEmpty()) {
-            Pair pair = virus.poll();
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = pair.x + dx[dir];
-                int ny = pair.y + dy[dir];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    if (new_board[nx][ny] == 0) {
-                        virus.add(new Pair(nx, ny));
-                        new_board[nx][ny] = 2;
-                    }
-                }
-            }
-        }
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (new_board[i][j] == 0)
-                    cnt++;
-            }
-        }
-        max_safe = Math.max(max_safe, cnt);
-    }
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//1. N, M 입력
+		st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		//2. 배열 입력
+		virusList = new ArrayList<>();
+		arr = new int[n][m];
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if(arr[i][j] == 2) {
+					virusList.add(new Point(i, j));
+				}
+			}
+		}
+		
+		//3. 백트래킹으로 벽 세울 곳 정하고, 3개 다 세웠다면 안전구역 개수 구하기
+		makeWall(0);
+		
+		System.out.println(maxSafetySize);
+	}
+	
+	public static void makeWall(int wall) {
+		if(wall == WALL_SIZE) {
+			spreadVirus();
+			return;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				//안전구역이면 벽 세워보고 넘어가기
+				if(arr[i][j] == 0) {
+					arr[i][j] = 1;
+					makeWall(wall + 1);
+					arr[i][j] = 0;
+				}
+			}
+		}
+	}
+	
+	public static void spreadVirus() {
+		ArrayDeque<Point> queue = new ArrayDeque<>();
+		boolean[][] visited = new boolean[n][m];
+		
+		int[][] tempArr = new int[n][m];
+	    
+		for (int i = 0; i < n; i++) {
+			tempArr[i] = arr[i].clone();
+	    }
+	      
+		
+		for(Point point : virusList) {
+			queue.add(point);
+			visited[point.x][point.y] = true;
+		}
+			
+		while(!queue.isEmpty()) {
+			Point cur = queue.poll();
+			
+			for(int dir = 0; dir < 4; dir++) {
+				int nx = cur.x + dx[dir];
+				int ny = cur.y + dy[dir];
+				
+				//범위 밖이면
+				if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+				
+				//벽이면 못감
+				if(tempArr[nx][ny] == 1) continue;
+				
+				if(visited[nx][ny]) continue;
+				
+				queue.offer(new Point(nx, ny));
+				visited[nx][ny] = true;
+				tempArr[nx][ny] = 2;
+			}
+		}
+		
+		int count = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(tempArr[i][j] == 0)
+					count++;
+			}
+		}
+		
+		maxSafetySize = Math.max(count, maxSafetySize);
+	}
 }
