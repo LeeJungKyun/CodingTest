f1dc3e37bf284b514174a11caa65cf3e652a5830
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-01 12:22:49 +0900

    [Silver II] Title: 색종이 만들기, Time: 88 ms, Memory: 13048 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,64 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/**
+ * @author 이정균
+ * 분할정복
+ *
+ */
 public class Main {
-    static int n;
-    static int[][] arr;
-    static int white, blue;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
-        white = 0;
-        blue = 0;
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int size = n;
-
-        cut(0, 0, size);
-
-        System.out.println(white);
-        System.out.println(blue);
-    }
-
-    public static void cut(int x, int y, int size) {
-        if (colorCheck(x, y, size)) {
-            if (arr[x][y] == 0) {
-                white++;
-            } else {
-                blue++;
-            }
-            return;
-        }
-
-        int newSize = size / 2;
-
-        cut(x, y, newSize);
-        cut(x, y + newSize, newSize);
-        cut(x + newSize, y, newSize);
-        cut(x + newSize, y + newSize, newSize);
-    }
-
-    public static boolean colorCheck(int x, int y, int size) {
-        int color = arr[x][y];
-
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
-                if (arr[i][j] != color) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	static int[][] arr;
+	static int n;
+	static int normalCount = 0, weirdCount = 0;
+	static BufferedReader br;
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		//--------------솔루션 코드를 작성하세요.---------------------------
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//N입력
+		n = Integer.parseInt(br.readLine());
+		
+		//배열 입력
+		arr = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());	 
+			}
+		}
+		
+		//정원 나누기 
+		divideGarden(0, 0, n);
+		
+		System.out.println(normalCount);
+		System.out.println(weirdCount);
+	}
+	
+	//분할정복으로 정원 나누기 모두 같은 색이 아니면 4등분해서 재귀 ㄱㄱ
+	
+	public static void divideGarden(int x, int y, int size) {
+		//모두 다 같은 색이면
+		if(isValid(x, y, size)) {
+			//정상 타일
+			if(arr[x][y] == 0)
+				normalCount++;
+			else weirdCount++;
+			return;
+		}
+		
+		
+		int newSize = size / 2;
+		divideGarden(x, y, newSize);
+		divideGarden(x, y + newSize, newSize);
+		divideGarden(x + newSize, y, newSize);
+		divideGarden(x + newSize, y + newSize, newSize);
+	}
+	
+	public static boolean isValid(int x, int y, int size) {
+		int std = arr[x][y];
+		for(int i = x; i < x + size; i++) {
+			for(int j = y; j < y + size; j++) {
+				//종류가 다르면 false
+				if(arr[i][j] != std)
+					return false;
+			}
+		}
+		//나머지는 true
+		return true;
+	}
 }
