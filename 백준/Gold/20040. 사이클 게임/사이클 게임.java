24ead0ef58c53b4ab0b9235441b66d336fec3843
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-01 12:24:18 +0900

    [Gold IV] Title: 사이클 게임, Time: 592 ms, Memory: 189476 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,54 +1,89 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * @author 이정균
+ * 
+ * n개의 유적지 ( 0 ~ n - 1)
+ * m개의 유적지 연결 정보에 따라 통로를 설치
+ * 사이클 최초 발생시 프로젝트 중단
+ * 
+ * 1. n, m 입력
+ * 2. parent 배열 초기화
+ * 3. m줄에 연결정보 입력
+ * 4. 입력 받은 출발지와 목적지가 연결이 안되어있다면 union 하고 다리개수 증가 -> m개 다 연결했으면 끝
+ * 5. find(from)과 find(to) 가 같으면 이제 사이클이 발생하는데 일단 다리개수는 증가시키고 끝
+ * 6. count가 m이면 사이클 없으니깐 0, 아니면 count 출력
+ */
 public class Main {
-
-  static int[] parent;
-  static int n, m;
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
-
-    parent = new int[n];
-    for (int i = 0; i < n; i++) {
-      parent[i] = i;
-    }
-
-    for (int i = 0; i < m; i++) {
-      st = new StringTokenizer(br.readLine());
-      int x = Integer.parseInt(st.nextToken());
-      int y = Integer.parseInt(st.nextToken());
-
-      if (find(x) == find(y)) {
-        System.out.println(i + 1);
-        return;
-      } else {
-        union(x, y);
-      }
-    }
-    System.out.println(0);
-  }
-
-  public static void union(int n1, int n2) {
-    int p1 = find(n1);
-    int p2 = find(n2);
-
-    if (p1 < p2) {
-      parent[p2] = p1;
-    } else {
-      parent[p1] = p2;
-    }
-  }
-
-  public static int find(int n) {
-    if (parent[n] == n) {
-      return n;
-    } else {
-      return parent[n] = find(parent[n]);
-    }
-  }
+	static ArrayList<Integer>[] graph;
+	static int[] parent;
+	static int n, m;
+	static BufferedReader br;
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		//--------------솔루션 코드를 작성하세요.---------------------------
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//1. n, m 입력
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		//2. parent 배열 초기화
+		parent = new int[n];
+		for(int i = 0; i < n; i++)
+			parent[i] = i;
+		
+		//3. m줄에 연결정보 입력
+		graph = new ArrayList[n];
+		for(int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		int count = 0;
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			graph[from].add(to);
+			graph[to].add(from);
+			
+			//4. 입력 받은 출발지와 목적지가 연결이 안되어있다면 union 하고 다리개수 증가
+			if(find(from) != find(to)) {
+				union(from, to);
+				count++;
+				//m개 다 연결했으면 끝
+				if(count == m) {
+					break;
+				}
+			}
+			//5. find(from)과 find(to) 가 같으면 이제 사이클이 발생하는데 일단 다리개수는 증가시키고 끝
+			else {
+				count++;
+				System.out.print(count);
+				return;
+			}
+		}
+		
+		//6. count가 m이면 사이클 없으니깐 0, 아니면 count 출력
+		System.out.print(count == m ? 0 : count);
+	}
+	
+	public static int find(int x) {
+		if(x == parent[x])
+			return x;
+		else return parent[x] = find(parent[x]);
+	}
+	
+	public static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		
+		if(x != y)
+			parent[y] = x;
+	}
 }
