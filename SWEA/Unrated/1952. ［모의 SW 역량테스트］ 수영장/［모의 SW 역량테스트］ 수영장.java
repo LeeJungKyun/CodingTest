fb7b6185e53d752b2e75bdcc8f7d6880a23bc33c
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-03 11:58:09 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 87 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,79 +1,69 @@
-/**
- * @author 이정균
- * 1년동안 각 달의 이용 계획을 수립하고 가장 적은 비용으로 수영장을 이용
- * 1일 이용권, 1달 이용권, 3달 이용권(11월 ~ 1월, 12월 ~ 2월 구매 불가), 1년 이용권 4종류
- * 각 달의 이동계획은 12칸 짜리 배열로 주어짐
- * 
- * 백트래킹 -> 각 달을 서로 다른 이용권으로 이용했을 때를 가정해서 재귀함수 돌리면 될 것 같음
- * 
- * 1. testCase 입력
- * 2. 각 테케별 필요한 변수들 초기화
- * 3. 이용권 별 요금 입력
- * 4. 이용 계획 입력
- * 5. findMinCost 함수 실행 (월, 플랜, 비용)
- * 	5-1. 현재까지 계산된 비용이 minCost보다 크면 바로 종료
- * 	5-2. 종료조건 month가 13이면 계산하고 minCost 갱신
- * 	5-3. 이번달에 이용 계획이 없으면 계산 안하고 넘어감
- *  5-4. 그렇지 않으면 1일권 사용시 계산, 1달권 사용시 계산, 3달권 이용시 month + 3 해서 계산(11월 미만때만)
- */
-
 import java.io.*;
 import java.util.*;
 
+/**
+ * @author 이정균
+ * 
+ * 이용권의 종류는 4개
+ * 12달이니깐 DP[13] ? 
+ * 
+ * 3달 이용권 골랐으면 다음 3달은 그 비용으로 채우고
+ * 1년짜리 이용권은 그냥 배열에 미리 다 채워놓기
+ */
+
 public class Solution {
-	static int testCase, minCost;
-	static int[] cost, plan;
+	static final int DAY = 0, MONTH = 1, THREE_MONTH = 2, YEAR = 3;
+	static int testCase;
+	static int[] memberShip, plan;
+	static int minCost;
+	static int[] dp;
 	static BufferedReader br;
-	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
-		//1. testCase 입력
+
 		testCase = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= testCase; tc++) {
-			//2. 각 테케별 필요한 변수들 초기화
-			cost = new int[4];
-			plan = new int[13];
+
+		for (int tc = 1; tc <= testCase; tc++) {
+			//변수 초기화
+			minCost = Integer.MIN_VALUE;
 			
-			//3. 이용권 별 요금 입력
+			//이용권 4개 입력
+			memberShip = new int[4];
 			st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < 4; i++)
-				cost[i] = Integer.parseInt(st.nextToken());
-			minCost = cost[3];
-			//4. 이용 계획 입력
+				memberShip[i] = Integer.parseInt(st.nextToken());
+			
+			//12개월 이용계획 입력 (편하게 1-based로 ㄱㄱ)
+			plan = new int[13];
 			st = new StringTokenizer(br.readLine());
 			for(int i = 1; i <= 12; i++)
 				plan[i] = Integer.parseInt(st.nextToken());
 			
-			//5. findMinCost 실행 (월, 비용)
-			findMinCost(1, 0);
+			//=== DP배열 선언해서 계산 시작 ===
 			
-				
+			dp = new int[13];
+			dp[0] = 0;
 			
+			//2월부터 DP채우기
+			for (int month = 1; month <= 12; month++) {
+			    // 1일권 사용
+			    dp[month] = dp[month - 1] + plan[month] * memberShip[DAY];
+
+			    // 1달권 사용
+			    dp[month] = Math.min(dp[month], dp[month - 1] + memberShip[MONTH]);
+
+			    // 3달권 사용
+			    if (month >= 3)
+			        dp[month] = Math.min(dp[month], dp[month - 3] + memberShip[THREE_MONTH]);
+			    else
+			        dp[month] = Math.min(dp[month], memberShip[THREE_MONTH]);
+			}
+			minCost = Math.min(dp[12], memberShip[YEAR]);
 			sb.append('#').append(tc).append(' ').append(minCost).append('\n');
 		}
 		System.out.println(sb);
-	}
-	
-	public static void findMinCost(int month,int curCost) {
-		//5-1. 현재까지 계산된 비용이 minCost보다 크면 바로 종료
-		if(curCost > minCost) return;
-		//5-2. 종료조건 month가 13이면 계산하고 minCost 갱신
-		if(month > 12) {
-			minCost = Math.min(curCost, minCost);
-			return;
-		}
-		
-		//5-3. 이번달에 이용 계획이 없으면 계산 안하고 넘어감
-		if(plan[month] == 0) 
-			findMinCost(month + 1, curCost);
-		else {
-			//5-4. 그렇지 않으면 1일권 사용시 계산, 1달권 사용시 계산, 3달권 이용시 month + 3 해서 계산(11월 미만때만)
-			findMinCost(month + 1, curCost + plan[month] * cost[0]);
-			findMinCost(month + 1, curCost + cost[1]);
-			findMinCost(month + 3, curCost + cost[2]);
-		}
-		
-		
 	}
 }
