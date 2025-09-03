e548c81758359254421f663d83a86505cb58c8dc
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-03 11:21:38 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 12116 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,26 +1,56 @@
-import java.util.*;
 import java.io.*;
-import java.math.*;
-
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] cost = new int[n][3];
+	static int n;
+	static int[][] cost, dp;
+	
+	static BufferedReader br;
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            cost[i][0] = Integer.parseInt(st.nextToken());
-            cost[i][1] = Integer.parseInt(st.nextToken());
-            cost[i][2] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 1; i < n; i++) {
-            cost[i][0] = Math.min(cost[i - 1][1], cost[i - 1][2]) + cost[i][0];
-            cost[i][1] = Math.min(cost[i - 1][0], cost[i - 1][2]) + cost[i][1];
-            cost[i][2] = Math.min(cost[i - 1][0], cost[i - 1][1]) + cost[i][2];
-        }
-        System.out.println(Math.min(cost[n - 1][0], Math.min(cost[n - 1][1], cost[n - 1][2])));
-    }
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		
+		//비용 입력 RGB
+		cost = new int[n][3];
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int color = 0; color < 3; color++) {
+				cost[i][color] = Integer.parseInt(st.nextToken());
+				
+			}
+		}
+		
+		//비용 계산
+		dp = new int[n][3];
+		for(int i = 0; i < 3; i++)
+			dp[0][i] = cost[0][i];
+		
+		for(int i = 1; i < n; i++) {
+			for(int j = 0; j < 3; j++) {
+				//R로 칠할때
+				if(j == 0) {
+					dp[i][j] = cost[i][j] +  Math.min(dp[i - 1][1], dp[i - 1][2]);
+				}
+				//G로 칠할때
+				else if(j == 1) {
+					dp[i][j] = cost[i][j] +  Math.min(dp[i - 1][0], dp[i - 1][2]);
+				}
+				//B로 칠할때
+				else {
+					dp[i][j] = cost[i][j] +  Math.min(dp[i - 1][0], dp[i - 1][1]);
+				}
+			}
+		}
+		
+		int min = Integer.MAX_VALUE;
+		for(int i = 0; i < 3; i++) {
+			min = Math.min(dp[n - 1][i], min);
+		}
+		
+		System.out.println(min);
+	}
 }
