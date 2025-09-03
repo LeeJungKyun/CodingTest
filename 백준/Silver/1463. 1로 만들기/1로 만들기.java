929fd97efd593894d87c25fad397b265729e04a8
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-03 11:27:50 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 11704 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,30 +1,45 @@
 import java.io.*;
+import java.util.*;
 
+/**
+ * @author 이정균
+ * 
+ * DFS로 경우 탐색하면서 1나오면 끝
+ * 
+ */
 public class Main {
-    static int ans = Integer.MAX_VALUE;
+	static int n;
+	static int result = Integer.MAX_VALUE;
+	static BufferedReader br;
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        toOne(n, 0);
-        System.out.println(ans);
-    }
-
-    static void toOne(int num, int cnt) {
-        if (num == 1) {
-            ans = Math.min(ans, cnt);
-            return;
-        }
-
-        if (cnt >= ans) return;
-
-        if (num % 3 == 0) {
-            toOne(num / 3, cnt + 1);
-        }
-        if (num % 2 == 0) {
-            toOne(num / 2, cnt + 1);
-        }
-        toOne(num - 1, cnt + 1);
-    }
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		
+		makeOne(n, 0);
+		
+		System.out.println(result);
+	}
+	
+	public static void makeOne(int num, int count) {
+		if(num == 1) {
+			result = Math.min(result, count);
+			return;
+		}
+        
+        //가지치기
+		if(count >= result)
+			return;
+		
+		if(num % 3 == 0)
+			makeOne(num / 3, count + 1);
+		
+		if(num % 2 == 0)
+			makeOne(num / 2, count + 1);
+		
+		makeOne(num - 1, count + 1);
+	}
 }
