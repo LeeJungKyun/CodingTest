e4ac2e60e2c0e0f8439810491bf20ba743142be9
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-08 11:07:28 +0900

    [Gold III] Title: 앱, Time: 92 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,50 +1,75 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * @author 이정균
+ * 
+ * N개의 앱이 활성화 되어있음
+ * 앱은 각각 m바이트의 메모리를 사용, 비활성화 했다가 다시 실행하면 c만큼 비용
+ * 
+ * 내가 새로 M바이트의 메모리가 필요함
+ * 현재 활성화되어있는 앱 중 몇개를 비활성화해서 비용은 최소로 M 메모리를 확보하고싶음
+ *
+ *
+ * 1. 앱의 개수 N과 필요한 메모리 M 입력
+ * 2. 메모리 입력
+ * 3. 비용 입력
+ * 4. DP의 크기는 총 비용만큼 선언하기
+ */
 public class Main {
-    static int n, m;
-    static int[] memory, cost;
-    static int[][] dp;
-    static int result = Integer.MAX_VALUE;
+	static int appCount, requiredMemory;
+	static int totalCost;
+	static int[] appMemory, appCost;
+	static int[] costDP;
+	
+	static BufferedReader br;
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//1. 앱의 개수 N과 필요한 메모리 M 입력
+		st = new StringTokenizer(br.readLine());
+		
+		appCount = Integer.parseInt(st.nextToken());
+		requiredMemory = Integer.parseInt(st.nextToken());
+		
+		//2. 메모리 입력
+		appMemory = new int[appCount];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < appCount; i++) {
+			appMemory[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		//3. 비용 입력
+		appCost = new int[appCount];
+		totalCost = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < appCount; i++) {
+			appCost[i] = Integer.parseInt(st.nextToken());
+			totalCost += appCost[i];
+		}
+		
+		//4. DP의 크기는 총 비용만큼 선언하기
+		costDP = new int[totalCost + 1];
+		costDP[0] = 0;
+		
+		for(int i = 0; i < appCount; i++) {
+			for(int j = totalCost; j >= appCost[i]; j--) {
+				costDP[j] = Math.max(costDP[j], costDP[j - appCost[i]] + appMemory[i]);
+			}
+		}
+		
+		int minCost = Integer.MAX_VALUE;
+		for(int i = 0; i <= totalCost; i++) {
+			if(costDP[i] >= requiredMemory) {
+				minCost = i;
+				break; 
+			}
+		}
+		
+		System.out.println(minCost);
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        memory = new int[n];
-        cost = new int[n];
-        dp = new int[n][10001];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            memory[i] = Integer.parseInt(st.nextToken());
-        }
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 0; i < n; i++) {
-            int curCost = cost[i];
-            int curMemory = memory[i];
-            for (int j = 0; j <= 10000; j++) {
-                if (i == 0) {
-                    if (j >= curCost)
-                        dp[i][j] = curMemory;
-                } else {
-                    if (j >= curCost) {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - curCost] + curMemory);
-                    } else dp[i][j] = dp[i - 1][j];
-                }
-
-                if (dp[i][j] >= m)
-                    result = Math.min(result, j);
-            }
-        }
-        System.out.println(result);
-    }
+	}
 }
