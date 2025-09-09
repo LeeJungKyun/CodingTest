0e9f15af50a02017deb518b0e794dd48199126a9
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-09 13:39:14 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 782 ms, Memory: 45,828 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -1,95 +1,150 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author 이정균
+ * N x N 정사각형의 게임판
+ * 1 ~ 5 블록
+ * 웜홀 6 ~ 10
+ * 블랙홀 -1
+ * 
+ * 1. 사이즈 입력
+ * 2. 배열 입력
+ * 	2-1. 주변은 5블록으로 둘러싸면 계산이 편해짐!
+ * 3. 0인 곳에서 방향 정해서 핀볼 게임 시작
+ */
 
 public class Solution {
-    static int[][] map; // NxN 크기의 배열 -1 ~ 10
-    static int N, ans; // N : 5 ~ 100사이 , ans 최대 점수를 의미
-    static int[] dr = { -1, 1, 0, 0 };
-    static int[] dc = { 0, 0, -1, 1 }; // 상하좌우
-    // 블록
-    static int[][] block = {
-            {0,1,2,3},
-            {1,3,0,2},
-            {3,0,1,2},
-            {2,0,3,1},
-            {1,2,3,0},
-            {1,0,3,2}
-    };
+	//클래스 선언부
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	//상수 선언부
+	static final int MAX = Integer.MAX_VALUE;
+	static final int MIN = Integer.MIN_VALUE;
 
-        int T = sc.nextInt();
-        for (int tc = 1; tc <= T; tc++) {
-            N = sc.nextInt();
-            ans = 0; // 점수가 0점일 수도 있음
-            map = new int[N + 2][N + 2];
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    map[i][j] = sc.nextInt();
-                }
-            } // 입력끝
+	//변수 선언부
+	static int testCase;
+	static int n;
+	static int maxScore = MIN;
+	static int[][] board;
+	static ArrayList<int[]>[] wormHoleList;
+	//상 하 좌 우
+	//0 1 2 3
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static int[][] blockDir = {
+			//0번 (그냥 그대로)
+			{0, 1, 2, 3},
+			//1번 블록
+			{1, 3, 0, 2},
+			//2번 블록
+			{3, 0, 1, 2},
+			//3번 블록
+			{2, 0, 3, 1},
+			//4번 블록
+			{1, 2, 3, 0},
+			//5번 블록
+			{1, 0, 3, 2}
+		};
+	
+	//입출력 선언부
+	static BufferedReader br;
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-            // 5번 블록
-            for (int i = 0; i <= N + 1; i++) {
-                map[i][0] = map[i][N + 1] = map[0][i] = map[N + 1][i] = 5;
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+
+		testCase = Integer.parseInt(br.readLine().trim());
+
+		for (int tc = 1; tc <= testCase; tc++) {
+			maxScore = MIN;
+			//1. 사이즈 입력
+			n = Integer.parseInt(br.readLine().trim());
+			
+			//2. 배열 입력
+			//TODO : 웜홀도 같이 저장
+			wormHoleList = new ArrayList[11];
+			for(int i = 0; i <= 10; i++)
+				wormHoleList[i] = new ArrayList<>();
+			
+			board = new int[n + 2][n + 2];
+			for(int i = 1; i <= n; i++) {
+				st = new StringTokenizer(br.readLine().trim());
+				for(int j = 1; j <= n; j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+					//웜홀 저장
+					if(board[i][j] >= 6)
+						wormHoleList[board[i][j]].add(new int[] {i, j});
+				}
+			}
+			
+			//2-1. 주변은 5블록으로 둘러싸면 계산이 편해짐!
+            for (int i = 0; i <= n + 1; i++) {
+            	board[i][0] = board[i][n + 1] = board[0][i] = board[n + 1][i] = 5;
             }
-
-            // 모든 곳/모든 방향 발사
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (map[i][j] == 0) {
-                        for (int d = 0; d < 4; d++) {
-                            shoot(i, j, d);
-                        } // 4방향 발사
-                    } // 빈블록쳌
-                } // 모든곳
-            }
-
-            System.out.println("#" + tc + " " + ans);
-
-        } // tc
-    }// main
-
-    private static void shoot(int stR, int stC, int d) {
-        int r = stR;
-        int c = stC;
-
-        int score = 0;
-
-        while (true) {
-            int nr = r + dr[d];
-            int nc = c + dc[d]; // 다음좌표
-
-            if ((nr == stR && nc == stC) || map[nr][nc] == -1) {
-                if (ans < score)
-                    ans = score;
-                return;
-            } // 시작좌표에 도달했거나, 블랙홀을 만나면 return
-
-            if (map[nr][nc] > 0) {
-                // 블록을 만났다.
-                if (map[nr][nc] < 6) {
-                    int blockNum = map[nr][nc];
-                    d = block[blockNum][d];
-                    score++;
-                } else {
-                    // 웜홀 이라는 뜻
-                    out: for (int i = 1; i <= N; i++) {
-                        for (int j = 1; j <= N; j++) {
-                            if ((nr != i || nc != j) && map[i][j] == map[nr][nc]) {
-                                nr = i;
-                                nc = j;
-                                break out;
-                            }
-                        }
-                    }//웜홀찾기
-                }//웜홀
-            }
-
-            r = nr;
-            c = nc;
-
-        } // 게임중
-
-    }
+			
+			//3. 0인 곳에서 방향 정해서 핀볼 게임 시작
+			for(int row = 1; row <= n; row++) {
+				for(int col = 1; col <= n; col++) {
+					if(board[row][col] == 0) {
+						for(int dir = 0; dir < 4; dir++) {
+							int score = startPinBall(row, col, dir);
+							maxScore = Math.max(maxScore, score);
+						}
+					}
+				}
+			}
+			sb.append('#').append(tc).append(' ').append(maxScore).append('\n');
+		}
+		System.out.println(sb);
+	}
+	
+	public static int startPinBall(int startRow, int startCol, int startDir) {
+		int currentScore = 0;
+		
+		int curRow = startRow;
+		int curCol = startCol;
+		int curDir = startDir;
+		
+		while(true) {
+			//다음 좌표 구하기
+			int nx = curRow + dx[curDir];
+			int ny = curCol + dy[curDir];
+			
+			//종료 조건 확인 (출발지로 복귀 또는 블랙홀 ㅠㅠ)
+			if((nx == startRow && ny == startCol) || board[nx][ny] == -1) {
+				return currentScore;
+			}
+			
+			//뭔가 이벤트가 발생한 경우
+			if(board[nx][ny] > 0) {
+				//블록 처리
+				if(board[nx][ny] < 6) {
+					int blockNum = board[nx][ny];
+					//블록만났으니 방향 바꿔주기
+					curDir = blockDir[blockNum][curDir];
+					currentScore++;
+				}
+				else {
+					//웜홀 처리
+					//나랑 같은 웜홀을 찾아줘
+					int wormHoleNum = board[nx][ny];
+					int[] nextWormHole;
+					
+					 if (wormHoleList[wormHoleNum].get(0)[0] == nx
+							 && wormHoleList[wormHoleNum].get(0)[1] == ny){
+					     nextWormHole = wormHoleList[wormHoleNum].get(1);
+					 } else {
+					     nextWormHole = wormHoleList[wormHoleNum].get(0);
+					 }
+					 nx = nextWormHole[0];
+					 ny = nextWormHole[1];
+				}
+			}
+			//이벤트 처리 끝
+			//그냥 옮기면 됩니다
+			curRow = nx;
+			curCol = ny;
+		}
+	}
 }
