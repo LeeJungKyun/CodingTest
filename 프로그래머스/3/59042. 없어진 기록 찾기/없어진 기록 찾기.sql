1665f6da5a1cad625f33e795aa691360a8a4b216
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-14 11:27:18 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,11 @@
--- 코드를 입력하세요
-SELECT AO.ANIMAL_ID, AO.NAME
-FROM ANIMAL_OUTS AO
-LEFT JOIN ANIMAL_INS AI
-ON AO.ANIMAL_ID = AI.ANIMAL_ID
-WHERE AI.ANIMAL_ID IS NULL;
+# 천재지변으로 인해 일부 데이터가 유실되었습니다.
+# 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_OUTS
+WHERE ANIMAL_ID NOT IN
+(
+    SELECT ANIMAL_ID
+    FROM ANIMAL_INS
+)
+ORDER BY ANIMAL_ID;
