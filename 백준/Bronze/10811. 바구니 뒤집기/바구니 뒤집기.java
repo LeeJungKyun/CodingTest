6c060e4bb03e626c5cdabb8d11a7ebf6e901dbea
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-15 11:16:47 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 68 ms, Memory: 11716 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,40 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+
+/**
+ * @author 이정균
+ * 1~N번 책
+ * M번에 걸쳐 구간을 선택해서 M번 반복한 이후 책장의 상태
+ */
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, m;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	static int n, m;
+	static int[] arr;
+	
+	static BufferedReader br;
+	static StringTokenizer st;
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n];
+		for(int i = 0; i < n; i++)
+			arr[i] = i;
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			//0번인덱스부터 하고 나중에 + 1
+			swap(start - 1, end - 1);
+		}
+		
+		for(int i = 0; i < n; i++)
+			System.out.print((arr[i] + 1) + " ");
+	}
+	
+	public static void swap(int startIdx, int endIdx) {
+		int mid = (endIdx - startIdx) / 2;
+		
+		for(int i = 0; i <= mid; i++) {
+			int temp = arr[startIdx + i];
+			arr[startIdx + i] = arr[endIdx - i];
+			arr[endIdx - i] = temp;
+		}
+	}
 
-        arr = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            arr[i] = i;
-        }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int left = Integer.parseInt(st.nextToken());
-            int right = Integer.parseInt(st.nextToken());
-
-            while (left < right) {
-                int temp = arr[left];
-                arr[left++] = arr[right];
-                arr[right--] = temp;
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
 }
