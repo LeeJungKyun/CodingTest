7a79dcd1d89ae94b97761deee83a2f202e64e194
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-09-17 17:33:35 +0900

    [Silver III] Title: 파도반 수열, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,31 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int T, N;
-    static Long[] arr;
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-
-        T = sc.nextInt();
-
-        for (int t = 0; t < T; t++) {
-            arr = new Long[101];
-            arr[0] = 0L;
-            arr[1] = 1L;
-            arr[2] = 1L;
-            arr[3] = 1L;
-            N = sc.nextInt();
-            System.out.println(Triangle(N));
-        }
-    }
-
-    private static long Triangle(int n) {
-        if (arr[n] == null) {
-            arr[n] = Triangle(n - 2) + Triangle(n - 3);
-        }
-        return arr[n];
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		
+		long[] dp = new long[101];
+		
+		dp[1] = 1;
+		dp[2] = 1;
+		dp[3] = 1;
+		
+		for(int i = 4; i <= 100; i++) {
+			dp[i] = dp[i-3] + dp[i-2];
+		}
+	
+		int testCase = Integer.parseInt(br.readLine());
+		
+		for(int i = 0;  i < testCase; i++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			sb.append(dp[n]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
