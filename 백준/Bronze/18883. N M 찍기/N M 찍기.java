e760a84365d0a1d864de7345dfb8be73f5e5da09
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2025-10-23 11:21:19 +0900

    [Bronze III] Title: N M 찍기, Time: 340 ms, Memory: 68684 KB -BaekjoonHub

---
+++ 백준/Bronze/18883. N M 찍기/N M 찍기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int num = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(j != m - 1)
+                    sb.append(num).append(" ");
+                else sb.append(num);
+                num++;
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
