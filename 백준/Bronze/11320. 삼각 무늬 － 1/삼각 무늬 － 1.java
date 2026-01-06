6893a581adab5e5f4baf91d5acb604893fb7461e
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2026-01-06 20:56:43 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			if(A % B == 0) System.out.println((A / B) * (A / B));
+			else System.out.println(((A / B) + 1) * ((A / B) + 1));
+		}
+	}
+
+}
